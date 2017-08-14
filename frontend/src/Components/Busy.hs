8178bba28c4cc4c4cbae604c4b17c4415ac44c54
Eugene Naumenko <eugene.naumenko@gmail.com>
2017-08-14 17:13:18 -0400

    busy fixes

--- frontend/src/Components/Busy.hs
+++ frontend/src/Components/Busy.hs
@@ -8,7 +8,7 @@ module Components.Busy where
 import qualified Data.VirtualDOM     as VD
 import Control.Applicative
 import Control.Concurrent            (forkIO, threadDelay)
-import Control.Monad                 (when)
+import Control.Monad                 (when, void)
 import Control.Monad.Fix             (MonadFix)
 import Data.Monoid                   ((<>))
 
@@ -19,29 +19,26 @@ import qualified Reflex.Host.App     as RHA
 import Types
 
 data BusyCmd = PushBusy | PopBusy
-  
+
 busyComponent :: (RHA.MonadAppHost t m, MonadFix m) => Sink Notification -> m (R.Dynamic t (VD.VNode l), Sink BusyCmd)
 busyComponent ntU = do
   (busyE :: R.Event t BusyCmd, busyU) <- RHA.newExternalEvent
   modelD <- R.foldDyn action 0 busyE
-  
-  subscribeToEvent (R.updated modelD) $ \v -> do
-    print $ "BusyFactor => " <> show v
-    when(v < 0) $ ntU (Error "BusyError" "BusyError") >> pure ()
-    pure ()  
-  
+
+  subscribeToEvent (R.updated modelD) $ \v ->
+    when(v < 0) $ void $ ntU (Error "BusyError" "BusyError")
+
   let view = render busyU <$> modelD
-  
+
   return (view, busyU)
-  
+
   where
     action c acc = case c of
       PushBusy -> acc + 1
-      PopBusy -> acc - 1
-      
+      PopBusy -> max 0 (acc - 1)
+
     container s x = block_ ("busy-wrapper " <> s) [x]
-    
+
     render busyU x
-      | x < 0 = mempty
-      | x == 0 = container "hide fadeOut" mempty
-      | otherwise = container "animated fadeIn" $ block [VD.text "Loading..."]
+       | x > 0 = container "animated fadeIn" $ block [VD.text $ "Loading... (" <> show x <> ")"]
+       | otherwise = container "hide fadeOut" mempty
